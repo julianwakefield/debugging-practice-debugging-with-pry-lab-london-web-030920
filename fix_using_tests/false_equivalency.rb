d97e1214c0f79c3a8bd12ebe12465bf3246301ab
Julian Wakefield <julianwakefield@outlook.com>
2020-03-08 22:08:42 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -11,12 +11,18 @@ def prompt_user
 end
 
 def selection(num)
-  if num = 1
+  num = 
      "YUM YUM MUNCH MUNCH MUNCH"
-  elsif num = 2
-    "HAM HAM HAM IN MY TUMMY"
+#  elsif num = 2
+   # "HAM HAM HAM IN MY TUMMY"
   end
  # binding.pry
+ 
+  #if num = 1
+   #  "YUM YUM MUNCH MUNCH MUNCH"
+  #elsif num = 2
+   # "HAM HAM HAM IN MY TUMMY"
+  #end
 end
 
 def runner
